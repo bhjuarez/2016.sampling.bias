8bba3fba3f20945ea60c7b749a46cf7224a3fad4
Bryan H. Juarez <scibry@Bryans-MacBook-Pro-3.local>
2016-08-24 14:15:38 -0500

    optimized func

--- estimate.lm.R
+++ estimate.lm.R
@@ -1,18 +1,47 @@
 require(ape)
+require(broom)
 require(geiger)
 require(geomorph)
 require(phytools)
 
-fun <- dat[, 1] ~ as.vector(dat[, 2])
-dat <- cbind(rnorm(50, 50, 125), rep(1, 50))
-dat <- rbind(dat, cbind(rnorm(50, 110, 90), rep(2, 50)))
-colnames(dat) <- c("length", "group")
+dat <- cbind(rnorm(50, 50, 125), rep(1, 50))  #Generate data 1
+dat <- rbind(dat, cbind(rnorm(50, 110, 90), rep(2, 50)))  #Generate data 2 and bind
+colnames(dat) <- c("length", "group")  #Assing column names
 dat <- as.data.frame(dat)
-estimate.lm(fun, dat, 200)
+test <- lm(dat[, 1] ~ dat [, 2])  #Run initial regression
 
-estimate.lm <- function(fun, dat, est, method){
-  method <- "lm"
-  if (method == "procD"){
+estimate.lm(test, 200)  #Run function
+
+estimate.lm <- function(mod.obj, est){
+  if (class(mod.obj) != "lm" & class(mod.obj) != "procD.lm"){  #Check if user provided correct object
+    stop("Please provide a lm or procD.lm model object")
+  }
+  
+  if (class(mod.obj) == "lm"){  #Implement lm
+    p <- glance(mod.obj)$p.value
+    if(p > 0.05){
+      stop("Regression is not significant")
+    }
+    
+    dat <- mod.obj$model
+    mean <- test[[1]][[1]]
+    sd <- sqrt(var(dat[, 1][which(dat[, 2] == 1, )]))  #Make multivariate ind and dep
+    n <- 0
+    stats <- matrix(ncol = 1, nrow = 0)
+    
+    while(n < est & p < 0.05){
+      n <- n + 1
+      sim <- rnorm(1, mean, sd)
+      sub.dat <- data.frame(sim, 1)
+      colnames(sub.dat) <- colnames(dat)
+      dat <- rbind(dat, sub.dat)
+      sub.p <- summary(lm(dat[, 1] ~ dat[, 2]))$coeff[2, 4] $make multivariate
+      stats <- rbind(stats, sub.p)	
+    }
+    return(dim(stats)[1])
+  }
+  
+  else {  #Implement procD.lm
     reg <- procD.lm(fun)
     p <- reg$aov.table[1, 7]
     
@@ -24,9 +53,9 @@ estimate.lm <- function(fun, dat, est, method){
     sd <- sqrt(var(dat[, 1][which(dat[, 2] == 1, )]))
     n <- 0
     i <- 0
-    stats <- matrix(ncol = 2, nrow = 0)
+    stats <- matrix(ncol = 1, nrow = 0)
     
-    while(n < est & p < 0.05){
+    while(n <= est & p < 0.05){
       n <- n + 1
       i <- i +1
       sim <- rnorm(1, mean, sd)
@@ -35,38 +64,10 @@ estimate.lm <- function(fun, dat, est, method){
       dat <- rbind(dat, sub.dat)
       sub.reg <- procD.lm(length ~ group, data = dat)
       p <- sub.reg$aov.table[1, 7]
-      stats <- rbind(stats, c(n, p))	
+      stats <- rbind(stats, p)	
     }
-    return(stats)
+    return(list(stats, dim(stats)[1]))
   }
-  else {
-    reg <- summary(lm(fun))
-    p <- reg$coeff[2, 4]
-    
-    if(p > 0.05){
-      stop("Regression is not significant")
-    }
-    
-    mean <- reg$coeff[1, 1]
-    sd <- sqrt(var(dat[, 1][which(dat[, 2] == 1, )]))
-    n <- 0
-    i <- 0
-    stats <- matrix(ncol = 2, nrow = 0)
-    
-    while(n < est & p < 0.05){
-      n <- n + 1
-      i <- i +1
-      sim <- rnorm(1, mean, sd)
-      sub.dat <- data.frame(sim, 1)
-      colnames(sub.dat) <- colnames(dat)
-      dat <- rbind(dat, sub.dat)
-      sub.reg <- summary(lm(dat[, 1] ~ as.factor(dat[, 2])))
-      p <- sub.reg$coeff[2, 4]
-      stats <- rbind(stats, c(n, p))	
-    }
-    return(stats)
-  }
-
 }
 
 
